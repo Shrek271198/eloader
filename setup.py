2540827f774454f91991cf7e44b0f8ea32ce9ad0
Devan <rehunathan@gmail.com>
2020-12-04 19:52:34 +0800

    Updated setup.py to fix eloader cli

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "eloader = eloader.cli.main",
+            "eloader = cli:main",
         ]
     },
 )
