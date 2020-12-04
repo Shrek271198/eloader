17bcb265e257a67675a95948ec39cbf6c2f1d78b
Devan <rehunathan@gmail.com>
2020-12-04 19:35:59 +0800

    Update setup.py entrypoints

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "eloader = cli.main",
+            "eloader = cli.main [standards, mel]",
         ]
     },
 )
